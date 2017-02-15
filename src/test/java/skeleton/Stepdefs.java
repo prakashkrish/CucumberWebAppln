1e4d4164e12c38ea7ea4b5ba87608a3ff0a1b9ff
KathirVelunathan <velunathan.lk@gmail.com>
2017-02-15 20:27:49 +0000

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -9,9 +9,9 @@ public class Stepdefs {
         belly.eat(cukes);
     }
     
-    @When("^I wait 1 hour$")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
+    @When("^I wait (\\d+) hour$")
+    public void I_wait_hour(int hour) throws Throwable {
         Belly belly = new Belly();
-        belly.eat(cukes);
+        belly.wait(hour);
     }
 }
