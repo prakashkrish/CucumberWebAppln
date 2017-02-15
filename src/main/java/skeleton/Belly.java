a74c86a9bccef0ab46872f9e1d2b9b8e9927c1d3
KathirVelunathan <velunathan.lk@gmail.com>
2017-02-15 20:29:23 +0000

    Update Belly.java

--- src/main/java/skeleton/Belly.java
+++ src/main/java/skeleton/Belly.java
@@ -2,6 +2,10 @@ package skeleton;
 
 public class Belly {
     public void eat(int cukes) {
-
+        System.out.println("Im eating "+cukes+" Cukes");
+    }
+    
+    public void wait(int hour) {
+        System.out.println("Im waiting for digestion");
     }
 }
