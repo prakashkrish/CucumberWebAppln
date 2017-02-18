88c23ae663665d75b87dc865d3700181c6bf3bbb
prakashkrish <josh.krishprakash@gmail.com>
2017-02-18 21:39:48 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -5,21 +5,32 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 
 public class Stepdefs {
-    @Given("^I have (\\d+) cukes in my belly$")
+/*    @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
-        belly.eat(cukes);
-    }
+        belly.eat(cukes);    */
     
-    @When("^I wait (\\d+) hour$")
+    Given(/^I am on the home page$/) do
+         visit "http://34.249.86.158:8181/hello/"
+        end
+    
+    
+/*    @When("^I wait (\\d+) hour$")
     public void I_wait_hour(int hour) throws Throwable {
         Belly belly = new Belly();
-        belly.wait(hour);
-    }
+        belly.wait(hour);  */
+
+    When(/^I click JSP page$/) do
+         find_link("JSP page").click
+        end
+   
     
-    @Then("^my belly should growl$")
+/*    @Then("^my belly should growl$")
     public void my_belly_should_growl() throws Throwable {
         Belly belly = new Belly();
-        belly.then();
-    }
+        belly.then();  */
+
+      Then(/^I should see the title "(.*?)"$/) do |title|
+          expect(page).to have_title(title)
+   
 }
